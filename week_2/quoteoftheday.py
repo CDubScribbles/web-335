7c51bac21b2321771b5330bf8ecf93a414106a25
EMFDeathPenguin <cliffordwsmith@gmail.com>
2026-06-14 15:46:33 -0400

    Week 2 Assignments

---
+++ week_2/quoteoftheday.py
@@ -0,0 +1,10 @@
+""" 
+    Title: quoteoftheday.py
+    Author: Clifford Smith
+    Date: 6/14/26
+    Description: Example file for outputting a literary quote.
+"""
+
+# Print a quote by Stephen King
+print("I am the literary equivalent of a Big Mac and fries.")
+print("-- Stephen King")
